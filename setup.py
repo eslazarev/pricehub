299caac62f0d6bab903d22953d7532736116878a
Evgenii Lazarev <elazarev@gmail.com>
2024-11-08 20:49:40 +0200

    fix build

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = "pricehub"
 
 setup(
     name=package_name,
-    version="0.0.3.2",
+    version="0.0.3.3",
     packages=find_packages(),
     author="Evgenii Lazarev",
     author_email="elazarev@gmail.com",
