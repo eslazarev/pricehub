a078b9818db737b3f571d6e56b52deff39f49679
Evgenii Lazarev <elazarev@gmail.com>
2024-11-18 09:02:45 +0200

    test binance

--- tests/brokers/test_broker_binance_spot.py
+++ tests/brokers/test_broker_binance_spot.py
@@ -2,15 +2,14 @@ from pricehub.models.get_ohlc_params import GetOhlcParams
 from pricehub.brokers.broker_binance_spot import BrokerBinanceSpot
 
 
-
 def test_fetch_data_return(
-    mock_binance_requests_get_with_side_effect, mock_binance_spot_api_response, get_broker_binance_spot_params
+    mock_binance_get_request_single, get_mock_binance_api_response, get_ohlc_binance_spot_params
 ):
     broker = BrokerBinanceSpot()
-    params = GetOhlcParams(**get_broker_binance_spot_params)
+    params = GetOhlcParams(**get_ohlc_binance_spot_params)
     data = broker.fetch_data(params)
 
-    assert data == mock_binance_spot_api_response
+    assert data == get_mock_binance_api_response
 
 
 def test_fetch_data_request_validation(
